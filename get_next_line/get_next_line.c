c7045024c7a1bfcf7746417858d5ca3146dd1130
Abdellah Azeroual <aazeroua@e2r3p13.1337.ma>
2019-05-15 00:28:13 +0000

    v2

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aazeroua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 16:36:19 by aazeroua          #+#    #+#             */
-/*   Updated: 2019/05/10 17:56:47 by aazeroua         ###   ########.fr       */
+/*   Updated: 2019/05/14 23:51:43 by aazeroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,31 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int		count_word(char **str, int fd)
-{
-	int i;
-
-	i = 0;
-	while (str[fd][i] != '\n' && str[fd][i] != '\0')
-		i++;
-	return (i);
-}
 int		get_next_line(const int fd, char **line)
 {
-	static char	*tab;
 	int 		i;
+	int			rd;
 	char		buff[BUFF_SIZE + 1];
-	while (read (fd, buff, BUFF_SIZE))
-	{	
-		if (ft_strchr(buff, '\n'))
+	static char	*tab;
+
+	if (fd < 0 || !line || !fd)
+		return (-1);
+	if (!tab)
+		tab = ft_strnew(0);
+	i = 0;
+	if (!ft_strchr(tab, '\n'))
+	{
+		while ((rd = read(fd, buff, BUFF_SIZE)))
 		{
-			i = 0;
-			while(buff[i] != '\n')
-				i++;
-			*line = ft_strsub(buff, 0, i);
+			buff[rd] = '\0';
+			tab = ft_strjoin(tab, buff);
+			if (ft_strchr(buff, '\n'))
+				break ;
 		}
 	}
+	while (tab[i] != '\n')
+		i++;
+	*line = ft_strsub(tab, 0, i);
+	tab = ft_strdup(&tab[i + 1]);
 	return (1);
-}
-int main()
-{
-	int i;
-	int fd = open("file_ds", O_RDONLY);
-	char *line;
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	/*get_next_line(fd, &line);
-	printf("%s\n", line);*/
-	return (0);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aazeroua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/22 16:36:32 by aazeroua          #+#    #+#             */
-/*   Updated: 2019/05/14 23:07:31 by aazeroua         ###   ########.fr       */
+/*   Created: 2019/05/12 04:47:44 by aazeroua          #+#    #+#             */
+/*   Updated: 2019/05/14 23:50:33 by aazeroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+#include <stdio.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+#include <fcntl.h>
 
-# include "libft/libft.h"
-
-int		get_next_line(const int fd, char **line);
-#endif
+int main()
+{
+	int fd = open("file_ds", O_RDONLY);
+	char *line;
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	free(line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	free(line);
+	return (0);
+}
