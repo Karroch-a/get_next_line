d58223d9f611ff60b6efac8ca1a6d51d0706cdf6
Abdellah Azeroual <aazeroua@e2r3p13.1337.ma>
2019-05-20 03:18:24 +0000

    vfinal

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,28 @@
 /*   By: aazeroua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 16:36:19 by aazeroua          #+#    #+#             */
-/*   Updated: 2019/05/17 19:18:09 by aazeroua         ###   ########.fr       */
+/*   Updated: 2019/05/20 02:10:20 by aazeroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
 
-int		get_next_line(const int fd, char **line)
+static void		ft_line(char **tab, int fd, int *i, char **line)
 {
-	int 		i;
+	while (tab[fd][*i] != '\n' && tab[fd][*i] != '\0')
+		*i = *i + 1;
+	*line = ft_strsub(tab[fd], 0, *i);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	int			i;
 	int			rd;
 	char		buff[BUFF_SIZE + 1];
 	static char	*tab[4864];
-	char *tm;
+	char		*tm;
 
-	if (fd < 1 || !line || fd > 4864 || BUFF_SIZE < 1)
+	if (fd < 0 || !line || fd > 4864 || read(fd, buff, 0) < 0)
 		return (-1);
 	if (!tab[fd])
 		tab[fd] = ft_strnew(0);
@@ -33,22 +39,11 @@ int		get_next_line(const int fd, char **line)
 		free(tab[fd]);
 		tab[fd] = tm;
 	}
-	if (!*tab[fd] || rd < 0)
-		return (rd < 0 ? -1 : 0);
-	while (tab[fd][i] != '\n' && tab[fd][i] != '\0')
-		    i++;
-	*line = ft_strsub(tab[fd], 0, i);
-	if (tab[fd][i] == '\n')
-	{
-		tm = ft_strdup(&tab[fd][i + 1]);
-		free(tab[fd]);
-		tab[fd] = tm;
-	}
-	else
-	{
-		tm = ft_strdup("\0");
-		free(tab[fd]);
-		tab[fd] = tm;
-	}
+	if (rd < 1 && !tab[fd][i])
+		return (rd);
+	ft_line(tab, fd, &i, line);
+	tm = ft_strdup(tab[fd][i] == '\n' ? &tab[fd][i + 1] : ft_strdup("\0"));
+	free(tab[fd]);
+	tab[fd] = tm;
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,16 @@
 /*   By: aazeroua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 16:36:32 by aazeroua          #+#    #+#             */
-/*   Updated: 2019/05/16 22:30:35 by aazeroua         ###   ########.fr       */
+/*   Updated: 2019/05/20 02:10:52 by aazeroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 10000
 
 # include "libft/libft.h"
 # include <fcntl.h>
-
 
 int		get_next_line(const int fd, char **line);
 #endif
